721075cd4806986234f0fb80c9e3139e32e5bd29
liarles <62631264+liarles@users.noreply.github.com>
2025-02-04 14:38:14 +0700

    Update lab17_1.cpp

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -4,28 +4,28 @@ using namespace std;
 
 int main(){
 	int nA,nB;
-	int *A,*B,*C;	
-	
+	int *A,*B,*C;
+
 	cout << "Length of A: ";
 	cin >> nA;
 	
-	A = new int;
+	A = new int[nA];
 	cout << "Input Array A: ";
 	for(int i=0; i < nA; i++) cin >> A[i];
 	
 	cout << "Length of B: ";
 	cin >> nB;
 	
-	B = new int;
+	B = new int[nB];
 	cout << "Input Array B: ";
 	for(int i=0; i < nB; i++) cin >> B[i];
 	
 	C = new int;
-	for(int i=0; i < nA; i++) C[i] = new int;
+	for(int i=0; i < nA; i++) C[i] = *new int[nA*nB];
 	
 	for(int i=0; i < nA; i++){
 		for(int j=0; j < nB; j++) {
-			C = A[i]*B[j];
+			C[i*nB+j] = A[i]*B[j];
 		}
 	}
 	
@@ -39,11 +39,13 @@ int main(){
 	for(int i=0; i < nA; i++){
 		cout << setw(5) << A[i] << " ";
 		for(int j=0; j < nB; j++) {
-			cout << setw(5) << C[i][j] << " ";
+			cout << setw(5) << C[i*nB+j] << " ";
 		}
 		cout << "\n";
 	}
-     delete A,B,C; 
+     delete A;
+     delete B;
+     delete C;
 
 	return 0;
 }
